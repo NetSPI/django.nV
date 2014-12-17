0b1d1c0068e12da04ef62e5253761c6d3b245976
cktricky <cktricky@gmail.com>
2014-12-17 18:15:53 -0500

    default URL now

--- projmanager/urls.py
+++ projmanager/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^$', 'taskManager.views.index', name='index'),
     url(r'^taskManager/', include('taskManager.urls', namespace="taskManager")),
     url(r'^admin/', include(admin.site.urls)),
+ 	
 ) 
