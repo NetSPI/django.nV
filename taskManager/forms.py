d46833b258e1034ef22626d165c4be895a6f9cd0
cktricky <cktricky@gmail.com>
2014-12-19 18:19:55 -0500

    fixed up registration page

--- taskManager/forms.py
+++ taskManager/forms.py
@@ -64,8 +64,10 @@ class AssignProject(forms.Form):
 
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput())
-
+	username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+	
 	class Meta:
 		model = User
 		fields = ('username', 'email', 'password')
