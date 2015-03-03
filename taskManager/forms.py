0b938a0cd4e01e2e6444165606e2bf56f661e1e2
Seth Law <e3c92bc8878c2593a7f3b741f59ce74af19521a9>
2015-03-02 17:37:58 -0700

    Added user profile update and change password pages.

--- taskManager/forms.py
+++ taskManager/forms.py
@@ -65,12 +65,14 @@ class AssignProject(forms.Form):
 
 class UserForm(forms.ModelForm):
 	username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	first_name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	last_name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
 	
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password')
+		fields = ('username', 'first_name', 'last_name', 'email', 'password')
 #look at mass assignments
 
 _Choices = (
