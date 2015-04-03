41e09950acca169797b242e8cb37fddd023962b5
Seth Law <e3c92bc8878c2593a7f3b741f59ce74af19521a9>
2015-04-03 14:58:25 -0600

    Bugfix - check/create uploads directory if needed.

--- taskManager/misc.py
+++ taskManager/misc.py
@@ -1,6 +1,9 @@
 import os
 
 def store_uploaded_file(title, f):
+	d = '%s/static/taskManager/uploads' % (os.path.dirname(os.path.realpath(__file__)))
+	if not os.path.exists(d):
+		os.makedirs(d)
 	with open('%s/static/taskManager/uploads/%s' % (os.path.dirname(os.path.realpath(__file__)), title), 'wb+') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
