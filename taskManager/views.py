7ec61e840c4337a9f82d20b778ff1d876e4b695d
Atticuss <Atticuss@github.com>
2015-04-02 21:00:35 -0400

    sqli added

--- taskManager/views.py
+++ taskManager/views.py
@@ -137,13 +137,16 @@ def upload(request, project_id):
 		if form.is_valid():
 			name = request.POST.get('name', False)
 			upload_path = store_uploaded_file(name, request.FILES['file'])
-		   
-			file = File(
-			name = name,
-			path = upload_path,
-			project = proj)
 
-			file.save()
+			curs = connection.cursor()
+			curs.execute("insert into taskManager_file ('name','path','project_id') values ('%s','%s',%s)"%(name,upload_path,project_id))
+		   
+			#file = File(
+			#name = name,
+			#path = upload_path,
+			#project = proj)
+
+			#file.save()
 
 			return redirect('/taskManager/' + project_id + '/', {'new_file_added':True})
 		else:
