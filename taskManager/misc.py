988a434effc22e90cd67758a62a6475034abb7b4
Cyrus Malekpour <cyrus@nvisium.com>
2015-07-14 16:40:38 -0400

    Added shell injection to file uploads

--- taskManager/misc.py
+++ taskManager/misc.py
@@ -4,7 +4,12 @@ def store_uploaded_file(title, f):
 	d = '%s/static/taskManager/uploads' % (os.path.dirname(os.path.realpath(__file__)))
 	if not os.path.exists(d):
 		os.makedirs(d)
-	with open('%s/static/taskManager/uploads/%s' % (os.path.dirname(os.path.realpath(__file__)), title), 'wb+') as destination:
-		for chunk in f.chunks():
-			destination.write(chunk)
+
+	# A1: Injection (shell)
+	# Let's avoid the file corruption race condition!
+	os.system ("mv " + f.temporary_file_path() + " " + "%s/static/taskManager/uploads/%s" % (os.path.dirname(os.path.realpath(__file__)), title))
+
+	#with open('%s/static/taskManager/uploads/%s' % (os.path.dirname(os.path.realpath(__file__)), title), 'wb+') as destination:
+	#	for chunk in f.chunks():
+	#		destination.write(chunk)
 	return '/static/taskManager/uploads/%s' % (title)
