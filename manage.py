b59e321158d0af36ccd2de9ed4f150e4d7717085
Cyrus Malekpour <cyrus@nvisium.com>
2015-08-13 20:12:10 -0400

    Added insecurity warning to all files w/ code (#42)

--- manage.py
+++ manage.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+
+#     _  _                        __   __
+#  __| |(_)__ _ _ _  __ _ ___   _ \ \ / /
+# / _` || / _` | ' \/ _` / _ \_| ' \ V / 
+# \__,_|/ \__,_|_||_\__, \___(_)_||_\_/  
+#     |__/          |___/                
+#
+#			INSECURE APPLICATION WARNING
+#
+# django.nV is a PURPOSELY INSECURE web-application
+# meant to demonstrate Django security problems
+# UNDER NO CIRCUMSTANCES should you take any code 
+# from django.nV for use in another web application!
+#
+
 import os
 import sys
 
