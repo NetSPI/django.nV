b783116c042204d31a1e571edb4ab9cfb3864b8d
Amos Boldor <amosboldor@gmail.com>
2017-02-05 19:50:25 -0800

    fix Security Misconfiguration and logged to vulnerability-report.md

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
 
 # A5: Security Misconfiguration
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
