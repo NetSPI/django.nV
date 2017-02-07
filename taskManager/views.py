f3632e2f164df0da75b9dafd224def691fcf9f06
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-06 17:30:22 -0800

    fix for upload view and report repair fo a4.

--- taskManager/views.py
+++ taskManager/views.py
@@ -171,30 +171,34 @@ def upload(request, project_id):
 
     if request.method == 'POST':
 
-        proj = Project.objects.get(pk=project_id)
-        form = ProjectFileForm(request.POST, request.FILES)
+        user = request.user
 
-        if form.is_valid():
-            name = request.POST.get('name', False)
-            upload_path = store_uploaded_file(name, request.FILES['file'])
+        if user.is_authenticated():
 
-            #A1 - Injection (SQLi)
-            curs = connection.cursor()
-            curs.execute(
-                "insert into taskManager_file ('name','path','project_id') values ('%s','%s',%s)" %
-                (name, upload_path, project_id))
+            proj = Project.objects.get(pk=project_id)
+            form = ProjectFileForm(request.POST, request.FILES)
 
-            # file = File(
-            #name = name,
-            #path = upload_path,
-            # project = proj)
+            if form.is_valid():
+                name = request.POST.get('name', False)
+                upload_path = store_uploaded_file(name, request.FILES['file'])
 
-            # file.save()
+                #A1 - Injection (SQLi)
+                curs = connection.cursor()
+                curs.execute(
+                    "insert into taskManager_file ('name','path','project_id') values ('%s','%s',%s)" %
+                    (name, upload_path, project_id))
 
-            return redirect('/taskManager/' + project_id +
-                            '/', {'new_file_added': True})
-        else:
-            form = ProjectFileForm()
+                # file = File(
+                #name = name,
+                #path = upload_path,
+                # project = proj)
+
+                # file.save()
+
+                return redirect('/taskManager/' + project_id +
+                                '/', {'new_file_added': True})
+            else:
+                form = ProjectFileForm()
     else:
         form = ProjectFileForm()
     return render_to_response(
